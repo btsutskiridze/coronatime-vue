460bd3da69dd50cea47afefa33e3c8d899fbf485
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-12-28 22:44:29 +0400

    feat: base container and input for login and registration pages

---
+++ src/components/layout/BaseContainer.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <div class="flex">
+    <div class="flex flex-col h-screen w-full px-[3%] py-[2%] lg:w-3/4">
+      <header>
+        <img src="@/assets/images/logo.svg" alt="logo" />
+      </header>
+      <slot></slot>
+    </div>
+    <div
+      class="h-screen hidden md:block w-2/4 bg-[url(@/assets/images/covid.png)] bg-no-repeat bg-center bg-cover"
+    ></div>
+  </div>
+</template>
---
+++ src/components/layout/form/BaseInput.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { Field, ErrorMessage } from "vee-validate";
+
+defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: false,
+    default: "text",
+  },
+  rules: {
+    type: String,
+    required: false,
+    default: "",
+  },
+});
+</script>
+
+<template>
+  <div>
+    <label for="email" class="block text-sm font-medium text-gray-700"
+      >Enter your {{ name }}</label
+    >
+    <div class="mt-1">
+      <Field
+        :id="name"
+        :name="name"
+        :type="type"
+        :rules="rules"
+        class="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+      />
+      <error-message :name="name" class="text-red-600 absolute text-xs pb-1" />
+    </div>
+  </div>
+</template>
